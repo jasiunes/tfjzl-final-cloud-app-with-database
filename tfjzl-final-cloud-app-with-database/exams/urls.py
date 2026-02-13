38ffcd8299307c0a78b8f07dfa6993a22826524c
jasiunes <69402983+jasiunes@users.noreply.github.com>
2026-02-13 09:58:49 +0100

    Update urls.py

--- tfjzl-final-cloud-app-with-database/exams/urls.py
+++ tfjzl-final-cloud-app-with-database/exams/urls.py
@@ -1,11 +1,9 @@
-
 from django.urls import path
 from . import views
 
-app_name = 'exams'
-
 urlpatterns = [
-    path('courses/<int:course_id>/', views.course_detail, name='course_detail'),
-    path('courses/<int:course_id>/submit/', views.submit_exam, name='submit_exam'),
-    path('submissions/<int:submission_id>/result/', views.exam_result, name='exam_result'),
+    path('<int:course_id>/submit/', views.submit, name='submit'),
+    path('courses/<int:course_id>/submissions/<int:submission_id>/result/', 
+         views.show_exam_result, 
+         name='exam_result'),
 ]
